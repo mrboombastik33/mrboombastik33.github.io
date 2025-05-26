e57d725129f1bccff87c498cb817fbc83bf94eaf
mrboombastik33 <timchik.oleksandr@gmail.com>
2025-05-26 22:12:10 +0300

    deploy config changed (test reasons)

--- lab4/vite.config.js
+++ lab4/vite.config.js
@@ -5,4 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "https://mrboombastik33.github.io/lab4/"
 })
